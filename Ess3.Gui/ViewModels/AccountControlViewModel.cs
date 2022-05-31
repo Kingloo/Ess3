cc95032b8670145d0aef7cdabc27393bed546682
Kingloo <k1ngloo@hotmail.com>
2022-05-31 02:57:16 +0200

    Commiting as backup

--- Ess3.Gui/ViewModels/AccountControlViewModel.cs
+++ Ess3.Gui/ViewModels/AccountControlViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
 using System.Threading.Tasks;
 using Amazon;
 using Ess3.Gui.Common;
